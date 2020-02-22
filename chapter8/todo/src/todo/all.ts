9537aed7f887153f8d8ea9f81ebe9d83870544cb
devminchan <minchan.dev@gmail.com>
2020-02-23 01:03:10 +0900

    Add all.ts (TodoGetAll method)

---
+++ chapter8/todo/src/todo/all.ts
@@ -0,0 +1,35 @@
+import {
+  APIGatewayProxyEvent,
+  Context,
+  Handler,
+  APIGatewayProxyResult
+} from 'aws-lambda';
+import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import { response } from '../common/helper';
+import { dynamodb, TABLE_TODOS } from '../database/dynamodb';
+import HttpStatusCode from "../common/httpStatusCode";
+
+export const all: Handler = async (
+  _event: APIGatewayProxyEvent,
+  _context: Context
+): Promise<APIGatewayProxyResult> => {
+  let data: DocumentClient.ScanOutput;
+
+  try {
+    data = await dynamodb
+      .scan({
+        TableName: TABLE_TODOS
+      })
+      .promise();
+  } catch (e) {
+    return response(HttpStatusCode.INTERNAL_SERVER_ERROR, {
+      result: false,
+      message: e.toString()
+    });
+  }
+
+  return response(HttpStatusCode.OK, {
+    result: true,
+    data
+  });
+}
