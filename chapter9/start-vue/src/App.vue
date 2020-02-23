6dd8adc2ed4cb86d0f7e20520578c2e4c40e71a5
devminchan <minchan.dev@gmail.com>
2020-02-23 15:03:45 +0900

    Add chapter9

---
+++ chapter9/start-vue/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span>{{ message }}</span><br/>
+    <span v-html="message"></span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HellWorld',
+  data: () => {
+    return {
+      message: '안녕하세요. <span style="color:red">Vue</span>입니다.',
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
