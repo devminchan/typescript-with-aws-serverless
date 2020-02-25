91f81346ad2081bb1690c38ecba31fd4dc5e4a80
devminchan <minchan.dev@gmail.com>
2020-02-25 22:01:30 +0900

    Add Props1.vue, Props2.vue

---
+++ chapter10/todo-app/src/components/Props1.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ name }} <br/>
+    <b>{{ userName }}</b>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({
+  props: {
+    name: String,
+  }
+})
+export default class PropsSample extends Vue {
+  get userName() {
+    return this.$props.name;
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ chapter10/todo-app/src/components/Props2.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    {{ name }}
+    <br />
+    <b>{{ userName }}</b>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from "vue-property-decorator";
+
+@Component
+export default class PropsSample extends Vue {
+  @Prop({ type: String, default: "기본값" })
+  private readonly name!: string; // 초기화 하지 않아도 됨
+
+  get userName() {
+    return this.$props.name;
+  }
+}
+</script>
+
+<style>
+</style>
