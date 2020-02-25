8e15a0631fcf67df84d489ef70b9cb989c15d720
devminchan <minchan.dev@gmail.com>
2020-02-25 22:10:53 +0900

    Create counter component with ts

--- chapter10/todo-app/src/App.ts
+++ chapter10/todo-app/src/App.ts
@@ -3,14 +3,14 @@ import Component from 'vue-class-component';
 import WithRender from './App.html';
 import './App.scss';
 
-import Props1 from '@/components/Props1.vue';
-import Props2 from '@/components/Props2.vue';
+// import Props1 from '@/components/Props1.vue';
+// import Props2 from '@/components/Props2.vue';
+import Methods from '@/components/Methods.vue';
 
 @WithRender
 @Component({
   components: {
-    Props1,
-    Props2
+    Methods
   }
 })
 export default class App extends Vue {}
---
+++ chapter10/todo-app/src/components/Methods.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    COUNT: {{ currentCount }}
+    <input type="button" @click="add()" value="더하기" />
+    <input type="button" @click="currentCount = 0" value="리셋" />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Method extends Vue {
+  private count: number = 0;
+
+  private add() {
+    this.count++;
+  }
+
+  private get currentCount() {
+    return this.count;
+  }
+
+  private set currentCount(value: number) {
+    this.count = value;
+  }
+}
+</script>
+
+<style>
+</style>
