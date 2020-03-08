1fca6ed3c193aa95f1caab1b22017f7c1ca4098e
devminchan <minchan.dev@gmail.com>
2020-03-08 17:08:29 +0900

    dark 테마 적용

--- chapter11/todo-list/src/plugins/vuetify.ts
+++ chapter11/todo-list/src/plugins/vuetify.ts
@@ -2,10 +2,22 @@ import Vue from 'vue';
 import Vuetify, { UserVuetifyPreset } from 'vuetify';
 
 import 'vuetify/dist/vuetify.min.css';
+import { colors } from 'vuetify/lib';
 
 const opts = {
   icons: {
     iconfont: 'mdi',
+  },
+  theme: {
+    dark: true,
+    themes: {
+      light: {
+        primary: colors.green.base,
+      },
+      dark: {
+        primary: colors.blue.lighten1,
+      },
+    },
   },
 } as Partial<UserVuetifyPreset>;
 
