c3103a1c0945e5950991857b1d95ae9ecee5f917
devminchan <minchan.dev@gmail.com>
2020-04-08 23:51:07 +0900

    relations로 관계에 있는 엔티티 조회

--- chapter16/src/index.ts
+++ chapter16/src/index.ts
@@ -1,24 +1,12 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
 import { User } from './entity/User';
-import { Profile } from './entity/Profile';
+// import { Profile } from './entity/Profile';
 
 createConnection()
   .then(async connection => {
-    const profile = await Profile.create({
-      name: 'john',
-      age: 22,
-    }).save();
-
-    // eslint-disable-next-line no-unused-vars
-    const user = await User.create({
-      email: 'tester@mail.com',
-      password: 'password',
-      profile,
-    }).save();
-
-    console.log('saved: ', profile);
-    console.log('saved: ', user);
+    const users = await User.find({ relations: ['profile'] });
+    console.log(users);
 
     await connection.close();
   })
